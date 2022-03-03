09ff154e7173bbeffe88fb41f0c83b4176b448c6
Dementisten <69969999+Dementisten@users.noreply.github.com>
2022-03-03 10:10:46 +0100

    Add files via upload

---
+++ Summa Program/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Summa_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath=@"C:\Users\Admin\Desktop\Summa Program\Summa.txt";
+            StreamReader sr = new StreamReader(filepath);
+
+
+            string rad;
+            int summa = 0;
+            int fem = 0;
+            while ((rad = sr.ReadLine()) != null ){
+                summa += int.Parse(rad);
+                if (rad.Contains("5")){
+                    fem++;
+                }
+            }
+
+            Console.WriteLine("Summan är: " + summa);
+            Console.WriteLine("Antal femmor: " + fem);
+            sr.Close();
+
+        }
+    }
+}
